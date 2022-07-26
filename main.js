7c5d11aee7d643e76f368d3d316e9cde452f2522
Jason Rothgeb <jrothgeb@gmail.com>
2022-07-26 12:58:56 -0500

    commit tree 0d6986204085a38f146182e27b8ce8bc6ae4ab26

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+ 
+const articleHearts = document.querySelectorAll(".like-glyph");
 
-// Your JavaScript code goes here!
+function hideError(hideE) {
+  const hideEr = document.getElementById('modal');
+  hideEr.hidden;
+} 
 
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert(serverMessage);
+      if (heart.innerText === FULL_HEART){
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      } else {
+        heart.innerText = FULL_HEART;
+        heart.classList.add('activated-heart');
+      };
+      
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
+
+// STEP 3: In order for the call to the server and the update of the screen to
+// work, we need to add a click event listener to the elements we identified in
+// STEP 1. That's Pillar 2, event handling. Uncomment this code:
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
